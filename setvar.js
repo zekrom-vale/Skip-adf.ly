a2867ba382a77e595284478dd2543795ae288657
Shawn <sgraven66@gmail.com>
2017-05-18 11:54:34 -0500

    Update setvar.js

--- setvar.js
+++ setvar.js
@@ -1,1 +1,7 @@
-var countdown= 9, count= 0,
+//How to force a varable to stay, regardless of other varable settings.  Like CSS !important
+var count, countdown;
+
+if (!countdown=0 & !count= 0){
+	countdown= 0;
+	count= 0;
+}

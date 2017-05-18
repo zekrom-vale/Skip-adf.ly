84ae75e3cdf6600d25c4fbd0ac44b3fb7ed7965e
Shawn <sgraven66@gmail.com>
2017-05-18 16:25:22 -0500

    Add files via upload

--- skip.js
+++ skip.js
@@ -1,6 +1,4 @@
-function skip{
 	var element = document.querySelector('meta[property="og:url"]');
 	var content = element && element.getAttribute("content");
 	window.location.replace(content);
-}
 //<meta property="og:url" content="http://console.developers.google.com/apis">

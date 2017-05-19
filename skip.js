d45fa32ad7282eb1289c8270ebdcfea53b498ef6
Shawn <sgraven66@gmail.com>
2017-05-19 16:04:56 -0500

    Add files via upload

--- skip.js
+++ skip.js
@@ -5,9 +5,9 @@ if (typeof content != "string"){
 }
 else{
 	if(content.includes("http://"|| "https://"|| "file:///"|| "file://")){
-		window.location.replace(content);
-		console.log("URL= "+content);
 		kill();
+		location.assign(content);
+		console.log("URL= "+content);
 	}
 	else{
 		console.log("Error! "+ content);
@@ -17,18 +17,15 @@ else{
 }
 
 function kill(){
-	window.onbeforeunload= null;
-	window.beforeunload= null;
-	window.onunload= null;
-	xhr.abort();
-	end;
-	stop;
-	kill;
+	document.onbeforeunload= null;
+	document.beforeunload= null;
+	document.onunload= null;
 	//Warning! this may remove other scripts
 	var srp = document.getElementsByTagName("script"),
 	i;
 	for (i= 0; i< srp.length; i++) {
 		document.getElementsByTagName("script").innerHTML= "var test= null; console.log('This is working to remove scripts')";
 	}
+	document.getElementsByTagName("html").innerHTML= "<html></html>";
 	console.log('kill.log')
 }

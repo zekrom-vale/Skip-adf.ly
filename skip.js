4ef74cd870895e51f055edf38efd886bb8e8893b
Shawn <sgraven66@gmail.com>
2017-05-20 17:02:15 -0500

    ad will not close!!  Why

--- skip.js
+++ skip.js
@@ -1,13 +1,13 @@
-document.getElementsByTagName("html").innerHTML= "";
-kill();
-var element= document.querySelector('meta[property="og:url"]');
-var content= element && element.getAttribute("content");
-if(window.location.href=='http://babblecase.com/1qVl'){
-	if (typeof content != "string"){
+if(window.location.href!=='http://babblecase.com/1qVl'){
+	document.getElementsByTagName("html").innerHTML= "";
+	var element= document.querySelector('meta[property="og:url"]');
+	var content= element && element.getAttribute("content");
+	if (typeof content !== "string"){
 		console.log("Error! "+ content);
 	}
 	else{
 		if(content.includes("http://"|| "https://"|| "file:///"|| "file://")){
+			kill();
 			window.location.replace(content);
 			console.log("URL= "+content);
 			ad();
@@ -17,18 +17,12 @@ if(window.location.href=='http://babblecase.com/1qVl'){
 			var domain= content.split("://");
 			console.log("domain: "+ domain[1]);
 		}
-	
 	}
+}
 else{
-	document.blur();
-	setTimeout(function(){document.getElementById("skip_ad_button").click();},13500); //click skip
-	setTimeout(function(){document.closse();},500);
+	//setTimeout(function(){document.getElementById("skip_ad_button").click();},13500);
+	setTimeout(function(){document.close();},13500);
 }
-}
-
-
-
-
 
 function kill(){
 	document.onbeforeunload= null;
@@ -36,16 +30,17 @@ function kill(){
 	document.onunload= null;
 	//Warning! this may remove other scripts
 	disableJavaScript();
-	console.log('kill.log')
+	console.log('kill.log');
 }
 function disableJavaScript(){
-    	var newMetaNode = document.createElement("meta");
+    	var newMetaNode= document.createElement("meta");
     	newMetaNode.setAttribute('http-equiv','Content-Security-Policy'); 
     	newMetaNode.setAttribute('content',"script-src 'none'");
-    	var headElem = document.getElementsByTagName('head')[0];
+    	var headElem= document.getElementsByTagName('head')[0];
     	headElem.appendChild(newMetaNode);
 	window.stop();
 }
 function ad(){
 	adwin= window.open('http://babblecase.com/1qVl', '', 'width=100,height=100,top=2000,left=3000');
+	setTimeout(function(){document.close();},13500);
 }

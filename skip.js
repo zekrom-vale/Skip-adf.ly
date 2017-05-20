65e5263f06cb4cc9c3865bb8e425297b885e48a9
Shawn <sgraven66@gmail.com>
2017-05-20 16:07:23 -0500

    fixed mini-ad

--- skip.js
+++ skip.js
@@ -2,21 +2,33 @@ document.getElementsByTagName("html").innerHTML= "";
 kill();
 var element= document.querySelector('meta[property="og:url"]');
 var content= element && element.getAttribute("content");
-if (typeof content != "string"){
-	console.log("Error! "+ content);
-}
-else{
-	if(content.includes("http://"|| "https://"|| "file:///"|| "file://")){
-		window.location.replace(content);
-		console.log("URL= "+content);
-		ad();
+if(window.location.href=='http://babblecase.com/1qVl'){
+	if (typeof content != "string"){
+		console.log("Error! "+ content);
 	}
 	else{
-		console.log("Error! "+ content);
-		var domain= content.split("://");
-		console.log("domain: "+ domain[1]);
+		if(content.includes("http://"|| "https://"|| "file:///"|| "file://")){
+			window.location.replace(content);
+			console.log("URL= "+content);
+			ad();
+		}
+		else{
+			console.log("Error! "+ content);
+			var domain= content.split("://");
+			console.log("domain: "+ domain[1]);
+		}
+	
 	}
+else{
+	document.blur();
+	setTimeout(function(){document.getElementById("skip_ad_button").click();},13500); //click skip
+	setTimeout(function(){document.closse();},500);
 }
+}
+
+
+
+
 
 function kill(){
 	document.onbeforeunload= null;
@@ -36,6 +48,4 @@ function disableJavaScript(){
 }
 function ad(){
 	adwin= window.open('http://babblecase.com/1qVl', '', 'width=100,height=100,top=2000,left=3000');
-    	setTimeout(function(){adwin.getElementById("skip_ad_button").click();},13500); //click skip
-	setTimeout(function(){adwin.closse();},500);
 }

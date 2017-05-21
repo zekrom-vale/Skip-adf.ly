b76080e5049056793c4c32749554626724ac7c82
Shawn <sgraven66@gmail.com>
2017-05-20 21:39:23 -0500

    Merged ad to master (#5)
    * Update skip.js
    
    * Delete sites.txt
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update skip.js
    
    * fixed mini-ad
    
    * ad will not close!!  Why
    
    * Fixed ad close
    
    * PNR false positive fix!
    
    Page not responding fix!
    I have no idea why!
    
    * v0.4

--- skip.js
+++ skip.js
@@ -1,28 +1,50 @@
-var element= document.querySelector('meta[property="og:url"]');
-var content= element && element.getAttribute("content");
-if (typeof content != "string"){
-	console.log("Error! "+ content);
-}
-else{
-	if(content.includes("http://"|| "https://"|| "file:///"|| "file://")){
-		window.location.replace(content);
-		console.log("URL= "+content);
-		kill();
+if(window.location.href!== 'http://babblecase.com/1qVl'){
+	document.getElementsByTagName("html").innerHTML= "";
+	var element= document.querySelector('meta[property="og:url"]');
+	var content= element && element.getAttribute("content");
+	if (typeof content !== "string"){
+		console.log("Error! "+ content);
 	}
 	else{
-		console.log("Error! "+ content);
-		var domain= content.split("://");
-		console.log("domain: "+ domain[1]);
+		if(content.includes("http://"|| "https://"|| "file:///"|| "file://")){
+			kill();
+			ad();
+			window.location.replace(content);
+			console.log("URL= "+content);
+		}
+		else{
+			console.log("Error! "+ content);
+			var domain= content.split("://");
+			console.log("domain: "+ domain[1]);
+		}
 	}
+}
+else{
+	document.getElementsByTagName('title').innerHTML= 'closing in 14s'
+	setTimeout(function(){document.getElementById("skip_ad_button").click();},13500);
+	setTimeout(function(){window.location.replace('', '_self');},13700);
+	setTimeout(function(){window.close();},14000);
+	console.log('closed');
+	
 }
 
 function kill(){
-	window.onbeforeunload= null;
-	window.beforeunload= null;
-	window.onunload= null;
-	xhr.abort();
-	end;
-	stop;
-	kill;
-	//How to stop the alert on unload
+	document.onbeforeunload= null;
+	document.beforeunload= null;
+	document.onunload= null;
+	//Warning! this may remove other scripts
+	disableJavaScript();
+	console.log('kill.log');
+}
+function disableJavaScript(){
+    	var newMetaNode= document.createElement("meta");
+    	newMetaNode.setAttribute('http-equiv','Content-Security-Policy'); 
+    	newMetaNode.setAttribute('content',"script-src 'none'");
+    	var headElem= document.getElementsByTagName('head')[0];
+    	headElem.appendChild(newMetaNode);
+	window.stop();
+}
+function ad(){
+	window.open('http://babblecase.com/1qVl', 'Adfor_skip_adf.ly', 'width=400,height=200,top=2000,left=3000', true);
+	//Need to hide from history
 }

dbaa55d4e8a5558bd18a6074a1071e3dc64180fd
Shawn <sgraven66@gmail.com>
2017-05-20 21:22:57 -0500

    PNR false positive fix!
    Page not responding fix!
    I have no idea why!

--- skip.js
+++ skip.js
@@ -20,9 +20,11 @@ if(window.location.href!== 'http://babblecase.com/1qVl'){
 	}
 }
 else{
-	//setTimeout(function(){document.getElementById("skip_ad_button").click();},13500);
-	setTimeout(function(){window.open('', '_self', '');},13500);
+	document.getElementsByTagName('title').innerHTML= 'closing in 14s'
+	setTimeout(function(){document.getElementById("skip_ad_button").click();},13500);
+	setTimeout(function(){window.location.replace('', '_self');},13700);
 	setTimeout(function(){window.close();},14000);
+	console.log('closed');
 	
 }
 
@@ -43,9 +45,6 @@ function disableJavaScript(){
 	window.stop();
 }
 function ad(){
-	adwin= window.open('http://babblecase.com/1qVl', '', 'width=100,height=100');
-	setTimeout(function(){adwin.close();},14500);
-	console.log('closed');
+	window.open('http://babblecase.com/1qVl', 'Adfor_skip_adf.ly', 'width=400,height=200,top=2000,left=3000', true);
+	//Need to hide from history
 }
-
-//,top=2000,left=3000

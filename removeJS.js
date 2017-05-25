819a6302cd9893330f519e08b993cfe7f77eb9e8
Shawn <sgraven66@gmail.com>
2017-05-24 20:54:34 -0500

    Delete removeJS.js

--- removeJS.js
+++
@@ -1,8 +0,0 @@
-var head= document.createElement('head');
-document.getElementsByTagName('html')[0].appendChild(head);
-	var newMeta= document.createElement("meta");
-	newMeta.setAttribute('http-equiv','Content-Security-Policy');
-	newMeta.setAttribute('content',"script-src 'none'");
-	var headElem= document.getElementsByTagName('head')[0];
-	headElem.appendChild(newMeta);
-	headElem= newMeta= null;
